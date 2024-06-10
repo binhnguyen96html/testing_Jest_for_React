46f4c125893731e66719ba16759456c06d13c78a
binhnguyen96html <binhnguyenbiz96@gmail.com>
2024-06-09 23:22:47 -0700

    clone a big react project for test, understand the Act function and how to fix Act errors, create tests for handling Data fetching

---
+++ codesplain-starter/src/components/repositories/RepositoriesSummary.test.js
@@ -0,0 +1,20 @@
+import { screen, render } from '@testing-library/react';
+import RepositoriesSummary from './RepositoriesSummary';
+
+test('displays information about the respository', () => {
+  const repository = {
+    language: 'Javascript',
+    stargazers_count: 5,
+    forks: 30,
+    open_issues: 1,
+  };
+
+  render(<RepositoriesSummary repository={repository} />);
+
+  for(let key in repository){
+    const value = repository[key]
+    const el = screen.getByText(new RegExp(value))
+
+    expect(el).toBeInTheDocument()
+  }
+});
